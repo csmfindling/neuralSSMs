068f8fc93ede68624c7f26cd6d4cbc61629fcf57
csmfindling <charles.findling@gmail.com>
2025-12-05 11:10:54 +0100

    learn to infer of weather prediction

--- WP/learn_to_infer.py
+++ WP/learn_to_infer.py
@@ -35,18 +35,17 @@ class Worker(torch.nn.Module):
         self.with_emission = with_emission
         if with_emission or train_with_emission:
             # load emission model
-            model_id = 121
-            #model_id = int(model_name.split('id')[-1])
+            model_id = int(model_name.split('id')[-1])
 
             # initialize emission RNN
             self.gru_emission = torch.nn.GRU(input_size=2, hidden_size=self.nb_units, batch_first=True, bias=True)
             self.W_output_emission = torch.nn.Parameter(torch.zeros(self.nb_units, 201))
 
             path_to_emission_networks = "../bandit/results/source/saved_models"
-            name_of_emission_network = f"banditGRU_id{model_id}_init_xavier_optim_Adam_episodeNbMax_50000_numUnits_32_rnnType_GRU_inputType_logodds"
+            name_of_emission_network = f"banditGRU_newinit_val_0_beta2_id{model_id}_init_xavier_optim_Adam_episodeNbMax_50000_numUnits_32_rnnType_GRU_inputType_logodds"
             files_to_load = sorted(glob.glob(path_to_emission_networks + "/" + name_of_emission_network + "/*"))            
             number_of_iterations_of_emission_model = np.max([int(f.split('-')[-1].split('.')[0]) for f in files_to_load])
-            idx_of_emission_model = np.argmax([int(f.split('-')[-1].split('.')[0]) for f in files_to_load])
+            idx_of_emission_model = np.argmax([int(f.split('-')[-1].split('.')[0]) == number_of_iterations_of_emission_model for f in files_to_load])
             print(f"loading the emission model with {number_of_iterations_of_emission_model} iterations")
             state_dict_emission = torch.load(files_to_load[idx_of_emission_model])
             for (key, val) in self.named_parameters():
@@ -131,7 +130,7 @@ class Worker(torch.nn.Module):
                 # update emission RNN state if needed
                 if self.with_emission and (not use_probabilitistic_reward):
                     # update emission RNN state
-                    pfiltering = (logalphas[:, i_trial] - torch.logsumexp(logalphas[:, i_trial], dim=-1, keepdims=True))
+                    pfiltering = (logpredict - torch.logsumexp(logpredict, dim=-1, keepdims=True))
                     selected_pfiltering = (
                         (pfiltering[:, 0] == pfiltering[:, 1]) * torch.randint(high=2, size=(self.env.num_tasks,)) + 
                         (pfiltering[:, 0] != pfiltering[:, 1]) * pfiltering.argmax(dim=1)
@@ -225,7 +224,7 @@ class Worker(torch.nn.Module):
             if episode_count % 10 == 0 and episode_count != 0:
                 
                 # Save model checkpoint
-                if episode_count % 500 == 0 and False:
+                if episode_count % 500 == 0:
                     model_dir = f"{self.model_path}/{self.model_name}"
                     os.makedirs(model_dir, exist_ok=True)
                     model_file = f"{model_dir}/model-{episode_count}.pth"
@@ -280,6 +279,12 @@ if __name__ == "__main__":
         with_emission=True,
         #train_with_emission=True
     )
-    
+
     self.load_model(trained_with_emission=False)
-    self.train()
+#    self.train()
+    ffs = [0.1] * 500 + [0.3] * 500
+    self.env.generate_test_task(num_tasks=1000, ffs=ffs, tau=0.05)
+    result = self.evaluate(use_probabilitistic_reward=False)
+    estimated_false_positive_rate = result['params_emission'][:, -1, :100].sum(axis=-1)
+    print(estimated_false_positive_rate[:500].mean())
+    print(estimated_false_positive_rate[500:].mean())
